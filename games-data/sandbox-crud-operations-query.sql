75e58ec396f3581f58bff86608d1b7870dbd083a
jmendez730 <109365107+jmendez730@users.noreply.github.com>
2022-11-24 01:26:23 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, string_value) VALUES(789456123, 1, 'I missed class lol');
+
+--UPDATE sandbox SET int32_value = 123456789 WHERE id = 214;
+
+--DELETE FROM sandbox WHERE id = 213;
